e3c192d6aa8e389a1a4d72b933cf8d8e690033bd
Rafayel Paremuzyan <rafopar@jlab.org>
2024-06-25 11:12:36 -0400

    Not final yet, but the framework now is operatinal. The namespace XYHodoTools now has multiple classes and functions. The XYHodoAnalyzer now can do analysis of the event just using the XYHODO::tdc bank, and can return different types of hits/crosses and other types of analyzed objects

--- include/XYHodoTools.h
+++ include/XYHodoTools.h
@@ -9,6 +9,157 @@
 #define XYHODOTOOLS_H
 
 
+// ===== Hipo headers =====
+#include <reader.h>
+namespace XYHodoTools {
+
+    struct DetElement {
+        short det; // The detector ID, 0, 1, etc
+        short layer; // The layer of the detector
+        short barID; // ID of the scintillator bar
+        short pmt; // PMT, 0 or 1
+
+        bool operator<(const DetElement& t) const {
+
+            return ( this->det < t.det) || (!(t.det < this->det) && (this->layer < t.layer)) || (!(t.det < this->det) && !(t.layer < this->layer)
+                    && (this->barID < t.barID)) || (!(t.det < this->det) && !(t.layer < this->layer) && !(t.barID < this->barID) && (this->pmt < t.pmt));
+        }
+
+    };
+
+    class XYHodoHit {
+    public:
+        XYHodoHit();
+        XYHodoHit(DetElement det_el);
+        XYHodoHit(DetElement det_el, int tdc_rise, int tdc_fall);
+
+        void SetTDC(int edge, int tdc);
+        void SetRiseTDC(int tdc);
+        void SetFallTDC(int tdc);
+
+        DetElement DetectorElement() const {
+            return fDetElement;
+        }
+
+        int RiseTime() const {
+            return fTDC_Rise;
+        }
+
+        int FallTime() const {
+            return fTDC_Fall;
+        }
+
+        bool IsInitialized() const {
+            return fInitialized;
+        }
+
+
+    private:
+        DetElement fDetElement;
+        int fTDC_Rise;
+        int fTDC_Fall;
+        bool fInitialized;
+
+    };
+
+    class XYHodoCross {
+    public:
+        XYHodoCross(std::shared_ptr<XYHodoHit>, std::shared_ptr<XYHodoHit>);
+
+        std::shared_ptr<XYHodoHit> ShortBarHit()const {
+            return fShortBarHit;
+        }
+        std::shared_ptr<XYHodoHit> LongBarHit()const {
+            return fShortBarHit;
+        }
+        
+        short ShortBarId()const;             // points to the id of the short bar
+        short LongBarId()const;              // points to the id of the long bar
+        int ShortBarRiseTime()const;         // Rise Time TDC of the short Bar
+        int LongBarRiseTime()const;          // Rise Time TDC of the long Bar
+        int ShortBarFallTime()const;         // Fall Time TDC of the short Bar
+        int LongBarFallTime()const;          // Fall Time TDC of the long Bar
+        
+        /* 
+         * The plan is to form crosses from hits that belong to the same PMT, 
+         * however in the future this might be reconsidered and we make crosses
+         * from hits of two separate PMTs, that is why more general methods
+         * are created below to access the PMT.
+         */
+        short PMTShort()const{
+            return fShortBarHit->DetectorElement().pmt;    
+        }
+        short PMTLong()const{
+            return fLongBarHit->DetectorElement().pmt;    
+        }
+        
+
+    private:
+        std::shared_ptr<XYHodoHit> fShortBarHit; // Points to the short Bar Hit
+        std::shared_ptr<XYHodoHit> fLongBarHit; // Points to the long Bar Hit
+    };
+
+    class XYHodoAnalyzer {
+    public:
+        XYHodoAnalyzer();
+        XYHodoAnalyzer(hipo::bank &HodoBank);
+        void PrintEvent() const;
+        
+    /* 
+     * Return methods for PMT1 and PMT2 crosses
+     */
+    const std::vector< XYHodoCross > *PMT1_Crosses(){
+        return &v_PMT1_Crosses;
+    }
+    const std::vector< XYHodoCross > *PMT2_Crosses(){
+        return &v_PMT2_Crosses;
+    }
+
+    /*
+     *  Return methods from PMT1 and PMT2
+     */
+    
+    const std::vector<std::shared_ptr< XYHodoHit> > *PMT1_Hits(){
+        return &v_PMT1_Hits;
+    }
+    const std::vector<std::shared_ptr< XYHodoHit> > *PMT2_Hits(){
+        return &v_PMT2_Hits;
+    }
+    
+    private:
+        std::vector< std::shared_ptr<XYHodoHit> > v_allHits;                    // Vector of all XYHodoHits
+        std::vector< std::shared_ptr<XYHodoHit> > v_PMT1_Hits;                  // Vector of all XYHodoHits from PMT1 only
+        std::vector< std::shared_ptr<XYHodoHit> > v_PMT2_Hits;                  // Vector of all XYHodoHits from PMT2 only
+        
+        std::vector< std::shared_ptr<XYHodoHit> > v_PMT1_ShortBar_Hits;         // Vector of short Bar XYHodoHits from PMT1 only
+        std::vector< std::shared_ptr<XYHodoHit> > v_PMT1_LongBar_Hits;          // Vector of long  Bar XYHodoHits from PMT1 only
+        std::vector< std::shared_ptr<XYHodoHit> > v_PMT2_ShortBar_Hits;         // Vector of short Bar XYHodoHits from PMT2 only
+        std::vector< std::shared_ptr<XYHodoHit> > v_PMT2_LongBar_Hits;          // Vector of long  Bar XYHodoHits from PMT2 only
+        
+        std::vector< XYHodoCross > v_PMT1_Crosses;                              // Crosses formed out of PMT1 hits
+        std::vector< XYHodoCross > v_PMT2_Crosses;                              // Crosses formed out of PMT2 hits
+        
+        std::vector< std::pair< std::shared_ptr<XYHodoHit>, std::shared_ptr<XYHodoHit> > > v_LR_Matches; // Vector of all matched XYHodoHits from both PMTs
+        //std::map< DetElement, XYHodoHit* > m_allHits;
+        std::map< DetElement, std::shared_ptr<XYHodoHit> > m_allHits;
+
+        int fBankSize; // The number of hits in the bank;
+        int fnAllHits; // The number of hits, i.e. the size of v_allHits, this number almost always is less than the BankSize.
+        int fn_PMT1Hits; // The number of hits from PMT1
+        int fn_PMT2Hits; // The number of hits from PMT2
+
+        hipo::bank *fHodoBank;
+
+        bool IsAnalyzed;
+        bool IsBankSet;
+
+        bool AnalyzeEvent();
+
+
+
+    };
+
+
+}
 
 #endif /* XYHODOTOOLS_H */
-
--- src/XYHodoTools.cc
+++ src/XYHodoTools.cc
@@ -1,2 +1,196 @@
 #include <XYHodoTools.h>
 
+#include <iostream>
+
+namespace XYHodoTools {
+
+    XYHodoHit::XYHodoHit() {
+
+        fInitialized = false;
+
+        fDetElement = DetElement{-1, -1, -1, -1};
+        fTDC_Rise = -1;
+        fTDC_Fall = -1;
+    }
+
+    XYHodoHit::XYHodoHit(DetElement det_el) : fDetElement{det_el}
+    {
+        fTDC_Rise = -1;
+        fTDC_Fall = -1;
+        fInitialized = false;
+    }
+
+    void XYHodoHit::SetTDC(int edge, int tdc) {
+        if (edge < 0 || edge > 1 || tdc < 0) {
+            std::cout << "Either edge or tdc values are not real." << std::endl;
+            std::cout << "edge = " << edge << std::endl;
+            std::cout << "tdc = " << edge << std::endl;
+            std::cout << "Exiting." << std::endl;
+            exit(1);
+        }
+
+        if (edge == 0) {
+            fTDC_Rise = tdc;
+        } else {
+            fTDC_Fall = tdc;
+        }
+    }
+
+    void XYHodoHit::SetRiseTDC(int tdc) {
+
+        if (tdc < 0) {
+            std::cout << "TDC can not be negative" << std::endl;
+            std::cout << "TDC = " << tdc << std::endl;
+            std::cout << "Exiting..." << std::endl;
+            exit(1);
+        }
+        fTDC_Rise = tdc;
+    }
+
+    void XYHodoHit::SetFallTDC(int tdc) {
+
+        if (tdc < 0) {
+            std::cout << "TDC can not be negative" << std::endl;
+            std::cout << "TDC = " << tdc << std::endl;
+            std::cout << "Exiting..." << std::endl;
+            exit(1);
+        }
+        fTDC_Fall = tdc;
+    }
+
+
+
+    XYHodoHit::XYHodoHit(DetElement det_el, int tdc_rise, int tdc_fall) : fDetElement{det_el}, fTDC_Rise{tdc_rise}, fTDC_Fall{tdc_fall}
+    {
+        fInitialized = true;
+    }
+
+    XYHodoAnalyzer::XYHodoAnalyzer() {
+
+        IsBankSet = false;
+        IsAnalyzed = false;
+    }
+
+    XYHodoAnalyzer::XYHodoAnalyzer(hipo::bank &HodoBank) {
+
+        fHodoBank = &HodoBank;
+        IsBankSet = true;
+
+        IsAnalyzed = AnalyzeEvent();
+
+    }
+
+    bool XYHodoAnalyzer::AnalyzeEvent() {
+
+        fBankSize = fHodoBank->getRows();
+
+        //std::cout<<"The Bank Size is "<<BankSize<<std::endl;
+
+        /* 
+         * First we loop over all elements of the TDC bank, then create all
+         * XYHodoHits
+         */
+        for (int i = 0; i < fBankSize; i++) {
+
+            short det = fHodoBank->getShort("sector", i);
+            short layer = fHodoBank->getShort("layer", i);
+            short component = fHodoBank->getShort("component", i);
+            short edge = fHodoBank->getShort("order", i);
+            int tdc = fHodoBank->getInt("TDC", i);
+
+            short pmt = component % 2;          // By definition pmt can not have values other than 0 or 1
+            short barID = component / 2;
+
+            DetElement detEl{det, layer, barID, pmt};
+
+            /*
+             *  Simple check, if the detector element is not in the map, then
+             * create a new element in the map.
+             */
+            if (m_allHits.find(detEl) == m_allHits.end()) {
+                m_allHits[detEl] = std::shared_ptr<XYHodoHit>(new XYHodoHit(detEl));
+            }
+
+            m_allHits.at(detEl)->SetTDC(edge, tdc);
+        }
+
+        /* Now let's loop over m_allHits elements and create different type of
+         * containers that will stor pointers to XYHodoHits.
+         * As an example PMT1 hits, PMT2 hits etc
+         */
+
+        for (auto it = m_allHits.begin(); it != m_allHits.end(); it++) {
+            //std::cout<<it->first.layer<<std::endl;
+            v_allHits.push_back(it->second);
+            if (it->first.pmt == 0) {
+                v_PMT1_Hits.push_back(it->second);
+                
+                if( it->first.layer == 0 ){
+                    v_PMT1_ShortBar_Hits.push_back(it->second);
+                }else if( it->first.layer == 1 ){
+                    v_PMT1_LongBar_Hits.push_back(it->second);
+                }
+                
+            }else{
+                v_PMT2_Hits.push_back(it->second);
+
+                if( it->first.layer == 0 ){
+                    v_PMT2_ShortBar_Hits.push_back(it->second);
+                }else if( it->first.layer == 1 ){
+                    v_PMT2_LongBar_Hits.push_back(it->second);
+                }
+            }
+        }
+        
+        
+        /*
+         *  Now let's make crosses out of those hits
+         */
+        
+        // ====== PMT1 crosses ======
+        for( auto shortBarHit: v_PMT1_ShortBar_Hits ){
+            for (auto longBarHit : v_PMT1_LongBar_Hits) {
+                v_PMT1_Crosses.push_back( XYHodoCross( shortBarHit, longBarHit ) );
+            }
+        }
+        
+        // ====== PMT2 crosses ======
+        for( auto shortBarHit: v_PMT2_ShortBar_Hits ){
+            for (auto longBarHit : v_PMT2_LongBar_Hits) {
+                v_PMT2_Crosses.push_back( XYHodoCross( shortBarHit, longBarHit ) );
+            }
+        }
+
+        return true;
+    }
+    
+    
+    XYHodoCross::XYHodoCross( std::shared_ptr<XYHodoHit> shortBarHit, std::shared_ptr<XYHodoHit> longBarHit) : fShortBarHit{shortBarHit}, fLongBarHit{longBarHit} {
+        
+    }
+    
+    short XYHodoCross::ShortBarId()const{
+        return fShortBarHit->DetectorElement().barID;
+    }
+
+    short XYHodoCross::LongBarId()const{
+        return fLongBarHit->DetectorElement().barID;
+    }
+    
+    int XYHodoCross::ShortBarRiseTime()const{
+        return fShortBarHit->RiseTime();
+    }
+
+    int XYHodoCross::ShortBarFallTime()const{
+        return fShortBarHit->FallTime();
+    }
+    
+    int XYHodoCross::LongBarRiseTime()const{
+        return fLongBarHit->RiseTime();
+    }
+
+    int XYHodoCross::LongBarFallTime()const{
+        return fLongBarHit->FallTime();
+    }
+    
+}
