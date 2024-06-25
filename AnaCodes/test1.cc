62799b86f67122e2be399824486e37d7deaa5d3d
Rafayel Paremuzyan <rafopar@jlab.org>
2024-06-25 11:13:58 -0400

    The AnaCodes/test1.cc is a simple test code that demonstrates using the XYHodoAnalyzer class, and plots various analyzed distributions

--- AnaCodes/test1.cc
+++ AnaCodes/test1.cc
@@ -9,6 +9,10 @@
 #include <writer.h>
 #include <dictionary.h>
 
+// ===== Hodo Tools ======
+
+#include <XYHodoTools.h>
+
 using namespace std;
 
 int main() {
@@ -33,8 +37,29 @@ int main() {
     TH1D h_bar_layer3_PMT1("h_bar_layer3_PMT1", "", NL3 + 1, 99.5, 100 + NL3 + 0.5);
     TH1D h_bar_layer3_PMT2("h_bar_layer3_PMT2", "", NL3 + 1, 99.5, 100 + NL3 + 0.5);
 
+    TH1D h_PMT1_N_PMT1_Crosses("h_PMT1_N_PMT1_Crosses", "", 19, -0.5, 18.5);
+    TH1D h_PMT1_N_PMT2_Crosses("h_PMT1_N_PMT2_Crosses", "", 19, -0.5, 18.5);
+    
+    TH2D h_PMT1_Cross_coors1("h_PMT1_Cross_coors1", "", 34, -0.5, 33.5, 13, -0.5, 12.5);
+    TH2D h_PMT2_Cross_coors1("h_PMT2_Cross_coors1", "", 34, -0.5, 33.5, 13, -0.5, 12.5);
+    
+    TH1D h_PMT1_Cross_DeltaT1("h_PMT1_Cross_DeltaT1", "", 200, -500., 500.);
+    TH1D h_PMT2_Cross_DeltaT1("h_PMT2_Cross_DeltaT1", "", 200, -500., 500.);
+    
+    TH2D h_PMT1_TimtOverThr_uniqueChan1("h_PMT1_TimtOverThr_uniqueChan1", "", 121, -0.5, 120.5, 200, 0., 1000.);
+    TH2D h_PMT2_TimtOverThr_uniqueChan1("h_PMT2_TimtOverThr_uniqueChan1", "", 121, -0.5, 120.5, 200, 0., 1000.);
+    
+    TH2D h_PMT1_TRise_UniqueChan1("h_PMT1_TRise_UniqueChan1", "", 121, -0.5, 120.5, 200, 0., 1050.);
+    TH2D h_PMT2_TRise_UniqueChan1("h_PMT2_TRise_UniqueChan1", "", 121, -0.5, 120.5, 200, 0., 1050.);
+    TH2D h_PMT1_TFall_UniqueChan1("h_PMT1_TFall_UniqueChan1", "", 121, -0.5, 120.5, 200, 0., 1050.);
+    TH2D h_PMT2_TFall_UniqueChan1("h_PMT2_TFall_UniqueChan1", "", 121, -0.5, 120.5, 200, 0., 1050.);
+    
+    TH2D h_PMT1_TimeOverThr_TRise1("h_PMT1_TimeOverThr_TRise1", "", 400, 500., 1050., 200, 0., 200);
+    TH2D h_PMT2_TimeOverThr_TRise1("h_PMT2_TimeOverThr_TRise1", "", 400, 500., 1050., 200, 0., 200);
+    
     hipo::reader reader;
-    reader.open("Data/decoded_2067_16.hipo");
+    //reader.open("Data/decoded_2067_16.hipo");
+    reader.open("Data/decoded_2067_All.hipo");
 
     hipo::dictionary factory;
 
@@ -59,56 +84,97 @@ int main() {
             }
 
             event.getStructure(bXYHodoTDC);
+            
+            XYHodoTools::XYHodoAnalyzer analyzer(bXYHodoTDC);
+            
+            const std::vector<XYHodoTools::XYHodoCross> *v_PMT1Crosses = analyzer.PMT1_Crosses();
+            const std::vector<XYHodoTools::XYHodoCross> *v_PMT2Crosses = analyzer.PMT2_Crosses();
+            
+            const std::vector< std::shared_ptr< XYHodoTools::XYHodoHit> > *v_PMT1_Hits = analyzer.PMT1_Hits();
+            const std::vector< std::shared_ptr< XYHodoTools::XYHodoHit> > *v_PMT2_Hits = analyzer.PMT2_Hits();
+            
+            h_PMT1_N_PMT1_Crosses.Fill( v_PMT1Crosses->size() );
+            h_PMT1_N_PMT2_Crosses.Fill( v_PMT2Crosses->size() );
+                        
+            for( auto cur_Cross : *v_PMT1Crosses ){
+                h_PMT1_Cross_coors1.Fill( cur_Cross.ShortBarId(), cur_Cross.LongBarId() );
+                h_PMT1_Cross_DeltaT1.Fill( cur_Cross.ShortBarRiseTime() - cur_Cross.LongBarRiseTime() );
+            }
 
-            int nHodoHits = bXYHodoTDC.getRows();
-            h_NHits1.Fill(nHodoHits);
+            for( auto cur_Cross : *v_PMT2Crosses ){
+                h_PMT2_Cross_coors1.Fill( cur_Cross.ShortBarId(), cur_Cross.LongBarId() );
+                h_PMT2_Cross_DeltaT1.Fill( cur_Cross.ShortBarRiseTime() - cur_Cross.LongBarRiseTime() );
+            }
+            
+            
+            for( auto curHit : *v_PMT1_Hits ){  
+                int unique_chan = 50*(curHit->DetectorElement().layer%2) + curHit->DetectorElement().barID;
+                h_PMT1_TimtOverThr_uniqueChan1.Fill(unique_chan, curHit->FallTime() - curHit->RiseTime() );
+                h_PMT1_TRise_UniqueChan1.Fill( unique_chan, curHit->RiseTime() );
+                h_PMT1_TFall_UniqueChan1.Fill( unique_chan, curHit->FallTime() );
+                
+                h_PMT1_TimeOverThr_TRise1.Fill( curHit->RiseTime(), curHit->FallTime() - curHit->RiseTime() );
+            }
+
+            for( auto curHit : *v_PMT2_Hits ){
+                int unique_chan = 50*(curHit->DetectorElement().layer%2) + curHit->DetectorElement().barID;
+                h_PMT2_TimtOverThr_uniqueChan1.Fill(unique_chan, curHit->FallTime() - curHit->RiseTime() );
+                h_PMT2_TRise_UniqueChan1.Fill( unique_chan, curHit->RiseTime() );
+                h_PMT2_TFall_UniqueChan1.Fill( unique_chan, curHit->FallTime() );
+                
+                h_PMT2_TimeOverThr_TRise1.Fill( curHit->RiseTime(), curHit->FallTime() - curHit->RiseTime() );
+            }
+            
+            
+//            int nHodoHits = bXYHodoTDC.getRows();
+//            h_NHits1.Fill(nHodoHits);
 
 
             //cout<<"*****NHits = "<<nHodoHits<<endl;
-            for (int ihit = 0; ihit < nHodoHits; ihit++) {
-
-                int sector = bXYHodoTDC.getInt("sector", ihit);
-                int layer = bXYHodoTDC.getInt("layer", ihit);
-                int component = bXYHodoTDC.getInt("component", ihit);
-                int edge = bXYHodoTDC.getInt("order", ihit);
-                int tdc = bXYHodoTDC.getInt("TDC", ihit);
-
-
-                int pmt = component % 2;
-                int barID = component / 2;
-
-                //cout<<"PMT = "<<pmt<<"   sec = "<<sector<<"    layer = "<<layer<<"   component = "<<component<<"    edge = "<<edge<<"    tdc = "<<tdc<<endl;
-
-                h_TDC_vsPMT1.Fill(pmt, tdc);
-
-                if (edge == 0) {
-                    h_TDC_vsPMT2.Fill(pmt, tdc);
-
-                    if (pmt == 0) {
-                        h_TDC_PMT1.Fill(tdc);
-
-                        if (layer == 0) {
-                            h_bar_layer1_PMT1.Fill(barID);
-                        } else if (layer == 1) {
-                            h_bar_layer2_PMT1.Fill(barID);
-                        } else if (layer == 2) {
-                            h_bar_layer3_PMT1.Fill(barID);
-                        }
-
-                    } else {
-                        h_TDC_PMT2.Fill(tdc);
-
-                        if (layer == 0) {
-                            h_bar_layer1_PMT2.Fill(barID);
-                        } else if (layer == 1) {
-                            h_bar_layer2_PMT2.Fill(barID);
-                        } else if (layer == 2) {
-                            h_bar_layer3_PMT2.Fill(barID);
-                        }
-
-                    }
-                }
-            }
+//            for (int ihit = 0; ihit < nHodoHits; ihit++) {
+//
+//                int sector = bXYHodoTDC.getInt("sector", ihit);
+//                int layer = bXYHodoTDC.getInt("layer", ihit);
+//                int component = bXYHodoTDC.getInt("component", ihit);
+//                int edge = bXYHodoTDC.getInt("order", ihit);
+//                int tdc = bXYHodoTDC.getInt("TDC", ihit);
+//
+//
+//                int pmt = component % 2;
+//                int barID = component / 2;
+//
+//                //cout<<"PMT = "<<pmt<<"   sec = "<<sector<<"    layer = "<<layer<<"   component = "<<component<<"    edge = "<<edge<<"    tdc = "<<tdc<<endl;
+//
+//                h_TDC_vsPMT1.Fill(pmt, tdc);
+//
+//                if (edge == 0) {
+//                    h_TDC_vsPMT2.Fill(pmt, tdc);
+//
+//                    if (pmt == 0) {
+//                        h_TDC_PMT1.Fill(tdc);
+//
+//                        if (layer == 0) {
+//                            h_bar_layer1_PMT1.Fill(barID);
+//                        } else if (layer == 1) {
+//                            h_bar_layer2_PMT1.Fill(barID);
+//                        } else if (layer == 2) {
+//                            h_bar_layer3_PMT1.Fill(barID);
+//                        }
+//
+//                    } else {
+//                        h_TDC_PMT2.Fill(tdc);
+//
+//                        if (layer == 0) {
+//                            h_bar_layer1_PMT2.Fill(barID);
+//                        } else if (layer == 1) {
+//                            h_bar_layer2_PMT2.Fill(barID);
+//                        } else if (layer == 2) {
+//                            h_bar_layer3_PMT2.Fill(barID);
+//                        }
+//
+//                    }
+//                }
+//            }
 
             //cin.ignore();
 
